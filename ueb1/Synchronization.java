b0e53198fd4521751419491d7c25128752547f4c
finnole12 <38003867+finnole12@users.noreply.github.com>
2024-11-10 23:50:46 +0100

    ueb2 commenting

--- ueb1/Synchronization.java
+++ ueb1/Synchronization.java
@@ -2,6 +2,13 @@ package ueb1;
 
 public class Synchronization {
    
+    /*
+     * starts two threads that decrement and increment a counter class respectively
+     * they are running at the same time.
+     * Thread1 which increments, gets a delay of 500ms to simulate elongated execution time
+     * This is done to showcase the interference effects that two threads can have when
+     * using the same object. When the object is synchronized the issue is fixed.
+     */
     public static void main(String[] args) {
 
         Counter counter = new Counter();
@@ -9,7 +16,10 @@ public class Synchronization {
         final Thread thread1 = new Thread() {
             public void run() {
                 counter.increment();
-                try { Thread.sleep(500); } catch (Exception e) { System.out.println(e.toString()); }
+
+                // sleep is used to simulate execution time
+                try { Thread.sleep(500); } 
+                catch (Exception e) { System.out.println(e.toString()); }
                 int newVal = counter.value();
                 System.out.println("Thread1 after inc:" + newVal);
             }
