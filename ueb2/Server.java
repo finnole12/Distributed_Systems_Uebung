2c33dae3546baa45bcba1af1cf7895d16fe8ffad
finnole12 <38003867+finnole12@users.noreply.github.com>
2024-11-11 00:22:06 +0100

    cleaned and commented ueb2

--- ueb2/Server.java
+++ ueb2/Server.java
@@ -4,19 +4,27 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.io.*;
 
+/*
+ * Server that accepts Client requests. Each client will be dealt with in a seperate
+ * Thread.
+ */
 public class Server {
 
     int port = 12345;
-    String filePath = "ueb2\\testseite.html";
+    String filePath = "ueb2\\files\\testseite.html";
     File file = new File(filePath);
 
-
+    /*
+     * Starts the server. Will continuisly listen for incoming client requests.
+     * 
+     */
     public void startServer() throws IOException {
         System.out.println("starting server...");
 
         try {
             ServerSocket serverSocket = new ServerSocket(port);
 
+            // Thread tasked with accepting incoming client requests
             final Thread thread = new Thread() {
                 public void run() {
                     int i = 0;
@@ -38,40 +46,41 @@ public class Server {
         }
     }
 
+    /*
+     * Starts a new Thread tasked with recieving messages from the clientSocket.
+     * If the input matches the filepath the file will be send to the client.
+     */
     private void startClientThread(Socket clientSocket, int num) {
         final Thread thread = new Thread() {
             public void run() {
                 try {
-                    BufferedReader iSR = new BufferedReader(new InputStreamReader (clientSocket.getInputStream()));
+                    BufferedReader iSR = new BufferedReader(
+                        new InputStreamReader (clientSocket.getInputStream())
+                    );
                     OutputStream oS = clientSocket.getOutputStream();
                     
-                    BufferedInputStream fileInputStream = new BufferedInputStream(new FileInputStream(filePath));
+                    BufferedInputStream fileInputStream = new BufferedInputStream(
+                        new FileInputStream(filePath)
+                    );
 
                     String b;
                     try {
                         while((b = iSR.readLine()) != null) {
-                            System.out.print(b);
-                            //if (b.equals("file")) {
-                                System.out.println("MATCH");
+                            System.out.print("Line: " + b);
+                            if (b.startsWith("GET") 
+                                && b.substring(5, b.indexOf(" ", 5)).equals(filePath.replaceAll("\\\\", "/"))) {
                                 byte[] fileBytes = new byte[(int) file.length()];
                                 fileInputStream.read(fileBytes);
                                 oS.write(fileBytes);
                                 oS.flush();
-                            //}
+                            }
                         }
                     } catch (IOException e) {
                         throw new RuntimeException(e);
                     }
-
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-
-                // file auswählen
-
-                // file senden
-
-                // connection closen
             }
         };
         thread.start();
